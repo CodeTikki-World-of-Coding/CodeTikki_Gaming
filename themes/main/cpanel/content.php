2d75e9c80c6489c05280be3b593a6ee778d37396
adminkhush <khushboosongara47@gmail.com>
2024-05-14 18:26:59 +0530

    creat Quiz set

--- themes/main/cpanel/content.php
+++ themes/main/cpanel/content.php
@@ -79,7 +79,7 @@
                     </div>
                 </nav>
                  <!-- Main Content -->
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 main">
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-1 main">
                     <div class="container mt-4" id="main-content">
                         
                     </div>
--- themes/main/cpanel/quizMaster/create_quiz/create-quiz.php
+++ themes/main/cpanel/quizMaster/create_quiz/create-quiz.php
@@ -7,16 +7,42 @@
         <div class="col-md-12"><label >Monthly</label></div>
         <div class="col-md-12"><label >Custom</label></div>
     </form>
-    <form id="secondForm" style="display:none">
+    <form id="showSecondFormBtn" style="display:none">
     <div class="col-md-12"><label><button onclick="showAutomatic(event)">Automatic</button></label></div>
     <div class="col-md-12"><label><button >manual</button></label></div>
 
 
 </form>
-<form id="thirdForm" style="display:none">
+<!-- <form id="thirdForm" style="display:none">
     <div class="quizQuestions" id="quizQuestions"></div>
 
-</form>
+</form> -->
+<!-- Modal structure -->
+<div class="modal" id="quizModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Quiz Questions</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="quizForm">
+                    <!-- Form inputs for quiz questions go here -->
+                    <div class="form-group">
+                        <h3>Quiz Set</h3>
+                        <label for="questionInput">Quiz Set Name </label>
 
+                    </div>
+                    <!-- Additional form inputs for options, etc. -->
+                    <button type="submit" class="btn btn-primary">Generate</button>
+                </form>
+            </div>
+            
+        </div>
+    </div>
+</div>
+    <div id="quizSet"></div>
 </div>
 <script src="themes/main/cpanel/quizMaster/create_quiz/js/creat-quiz_script.js"></script>
