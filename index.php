fb4dc8ce39ef38ffcbf75bfedc0da03466bfdaae
unknown <khushboosongara47@gmail.com>
2024-06-07 17:57:57 +0530

    work on Home page

--- index.php
+++ index.php
@@ -1,99 +1,284 @@
-<?php
-include 'setting.php';
-
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = isset($_POST['text']) ? $_POST['text'] : '';
-    $password = isset($_POST['password']) ? $_POST['password'] : '';
-
-    function fetchUserID($name, $pdo) {
-      try {
-          $stmt = $pdo->prepare("SELECT Id FROM User WHERE username = :username ");
-          $stmt->bindParam(':username', $name);
-          $stmt->execute();
-          $user_id = $stmt->fetchColumn();
-          return $user_id;
-      } catch(PDOException $e) {
-          echo "Error fetching user ID: " . $e->getMessage();
-          return null;
-      }
-  }
-  $user_id = fetchUserID($username, $pdo);
-  $_SESSION['user_id'] = $user_id;
-  //for make a email session 
-  
-    try {
-        $verifyStmt = $pdo->prepare("SELECT * FROM User WHERE username = :username ");
-        $verifyStmt->bindParam(':username', $username);
-        $verifyStmt->execute();
-        $user = $verifyStmt->fetch(PDO::FETCH_ASSOC);
-  
-        if ($user) {
-          $hashed_password_from_db = $user['password'];
-      
-          if (password_verify($password, $hashed_password_from_db)) {
-              $_SESSION['userid'] = $user['Id'];
-              $_SESSION['username'] = $user['username'];
-              $_SESSION['mailid'] = $user['email'];
-              
-              // Redirect to home.php
-              header("Location: home.php");
-              exit();
-          } else {
-              $error_message = "Invalid username or password.";
-          }
-      } else {
-          $error_message = "User not found.";
-      }
-      
-    } catch (\PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-}
-?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Login Page</title>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-<link href="./themes/css/style.css" rel="stylesheet">
-</head>
-<div class="container-fluid login-page">
-  <div class="row">
-    <div class="col-md-6 logo-pic"><img src="./themes/images/image-removebg-preview (50) 2.png" alt=""></div>
-    <div class="col-lg-6 offset-lg-3">
-      <div class="login-box">
-        <h2 class="text-center mb-4 login-heading">Login Form</h2>
-        <?php if (isset($error_message)) { ?>
-        <p><?php echo $error_message; ?></p>
-    <?php } ?>
-        
-        <form method="post" action="home.php">
-          <div class="form-group form-group_login">
-            <input type="text" id="text" name="text" class="form-control" placeholder="Username">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="./themes/css/style.css" rel="stylesheet">
+
+    <title>CodeTikki</title>
+  </head>
+  <body>
+<div class="container-fluid mainHomePage" id="mainHomePage">
+  <nav class="navbar navbar-expand-lg p-4 navHomePage">
+    <div class="container-fluid ">
+      <a class="navbar-brand" href="#"><img src="themes/images//LARGE 2.svg" alt="Codetikki">CodeTikki</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul> -->
+      <!-- <form class="d-flex">
+
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
+      <ul class="position-absolute end-0 navbar-nav me-5 mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><img src="themes/images/solar_home-bold.svg" alt=""> Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="login.php">Login/Register</a>
+        </li>
+      </div>
+    </div>
+  </nav>
+  <div class="container-fluid main_page">
+    <div class="col-md-12 firstCol">
+      <div class="content">
+        <h3>Welcome to CodeTikki</h3>
+        <p>Test Your Coding Skills with Fun and Challenging Quizzes</p>
+        <button>Start Now</button>
+      </div>
+    </div>
+    <div class="col-md-12 secondCol">
+      <h3>OUR CODING QUIZZES</h3>
+      <div class="row">
+        <div class="col-md-4 subColCodeLogic"></div>
+        <div class="col-md-5 subColCodeLogicShowdown">
+          <h3>Code Logic ShowDown</h3>
+          <ul>
+            <li>
+              <img src="themes/images/Group 48095900.svg" alt="">
+              <div class="beginnerText">
+                <h4> Beginner Quizzes</h4>
+                <p>Start with the basics and build your coding skills.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/Group 48095901.svg" alt="">
+              <div class="intermediateQuizes">
+                <h4>Intermediate Quizzes</h4>
+                <p>Challenge yourself with more advanced coding problems.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/vghvs 1.svg" alt="">
+              <div class="expertQuizzes">
+                <h4>Expert Quizzes</h4>
+                <p>Test your skills with the toughest coding challenges.</p>
+              </div>
+            </li>
+          </ul>
+        </div>        
+      </div>
+      <div class="row">
+        <div class="col-md-5 subColByteBattleRoyalePic">
+          <h3>Byte Battle Royale</h3>
+          <ul>
+            <li>
+              <img src="themes/images/Group 48095900.svg" alt="">
+              <div class="beginnerText">
+                <h4> Beginner Quizzes</h4>
+                <p>Start with the basics and build your coding skills.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/Group 48095901.svg" alt="">
+              <div class="intermediateQuizes">
+                <h4>Intermediate Quizzes</h4>
+                <p>Challenge yourself with more advanced coding problems.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/vghvs 1.svg" alt="">
+              <div class="expertQuizzes">
+                <h4>Expert Quizzes</h4>
+                <p>Test your skills with the toughest coding challenges.</p>
+              </div>
+            </li>
+          </ul>
+        </div> 
+        <div class="col-md-4 subColByteBattleRoyale"></div>       
+      </div>
+      <div class="row">
+        <div class="col-md-4 subColBrainyLegend"></div>
+        <div class="col-md-5 subColBrainyLegendleague">
+          <h3>Brainy Legends League</h3>
+          <ul>
+            <li>
+              <img src="themes/images/Group 48095900.svg" alt="">
+              <div class="beginnerText">
+                <h4> Beginner Quizzes</h4>
+                <p>Start with the basics and build your coding skills.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/Group 48095901.svg" alt="">
+              <div class="intermediateQuizes">
+                <h4>Intermediate Quizzes</h4>
+                <p>Challenge yourself with more advanced coding problems.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/vghvs 1.svg" alt="">
+              <div class="expertQuizzes">
+                <h4>Expert Quizzes</h4>
+                <p>Test your skills with the toughest coding challenges.</p>
+              </div>
+            </li>
+          </ul>
+        </div>        
+      </div>
+      <div class="row">
+        <div class="col-md-5 subColCodeCraftQuizSeries">
+          <h3>Code Craft Quiz Series</h3>
+          <ul>
+            <li>
+              <img src="themes/images/Group 48095900.svg" alt="">
+              <div class="beginnerText">
+                <h4> Beginner Quizzes</h4>
+                <p>Start with the basics and build your coding skills.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/Group 48095901.svg" alt="">
+              <div class="intermediateQuizes">
+                <h4>Intermediate Quizzes</h4>
+                <p>Challenge yourself with more advanced coding problems.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/vghvs 1.svg" alt="">
+              <div class="expertQuizzes">
+                <h4>Expert Quizzes</h4>
+                <p>Test your skills with the toughest coding challenges.</p>
+              </div>
+            </li>
+          </ul>
+        </div> 
+        <div class="col-md-4 subColCodeCraftQuiz"></div>       
+      </div>
+      <div class="row">
+        <div class="col-md-4 subAlgorithemArena"></div>
+        <div class="col-md-5 subAlgorithemArenaQuiz">
+          <h3>Algorithm Arena</h3>
+          <ul>
+            <li>
+              <img src="themes/images/Group 48095900.svg" alt="">
+              <div class="beginnerText">
+                <h4> Beginner Quizzes</h4>
+                <p>Start with the basics and build your coding skills.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/Group 48095901.svg" alt="">
+              <div class="intermediateQuizes">
+                <h4>Intermediate Quizzes</h4>
+                <p>Challenge yourself with more advanced coding problems.</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/vghvs 1.svg" alt="">
+              <div class="expertQuizzes">
+                <h4>Expert Quizzes</h4>
+                <p>Test your skills with the toughest coding challenges.</p>
+              </div>
+            </li>
+          </ul>
+        </div>        
+      </div>
+      <div class="row subjects">
+        <div class=" col-md-5 python"><img src="themes/images/Group 48095922.png" alt=""></div>
+        <div class="col-md-4 javascriptFoundation"><img src="themes/images/Group 48095917.png" alt=""></div>
+        <div class="col-md-4 algorithem"><img src="themes/images/Group 48095918.png" alt=""></div>
+        <div class="col-md-4 dataStructure"><img src="themes/images/Group 48095928.png" alt=""></div>
+      </div>
+      <div class="row QuestionSection">
+         <h3>FREQUENTLY ASKED QUESTIONS </h3>
+         <hr>
+         <div class="row questionAnswereSection">
+          <div class="col-md-5 firstQuestion">
+
           </div>
-          <div class="form-group form-group_login">
-            <input type="password" id="password" name="password" class="form-control" placeholder="password">
-          </div>
-          <div class="form-group form-group_login">
-          <button type="submit" class="btn btn_login">Signin</button>
-          <label class="or" >OR</label>
-                        <button type="button" class="btn btn-register "><a href="registration.php" >Register</a></button>
-                    
-</div>
-        </form>
+          <div class="col-md-5 secondQuestion"></div>
+          <div class="col-md-5 thirdQuestion"></div>
+          <div class="col-md-5 fourthQuestion"></div>
+          <div class="col-md-5 fifthQuestion"></div>
+          <div class="col-md-5 sixthQuestion"></div>
+         </div>
+      </div>
+      <div class="ContactArea">
+        <div class="col-md-4 Contact">
+          <img src="themes/images/Group 48095943.png" alt="">
+        </div>       
+        <div class="col-md-4 ContactGroup">
+          <h3>Contacts</h3>
+          <ul>
+            <li>
+              <img src="themes/images/Vector.png" alt="">
+              <div class="email">
+                <h4> Email</h4>
+                <p>support@codetkki.com</p>
+              </div>
+            </li>
+            <li>
+              <img src="themes/images/Vector (1).png" alt="">
+              <div class="name">
+                <h4>Name</h4>
+                <p>CodeTikki Support</p>
+              </div>
+            </li>
+            
+          </ul>
+        </div> 
       </div>
     </div>
   </div>
+  <footer class="footer ">
+    <div class="row">
+      <div class="col-md-5 ps-5">
+          <a href="#"><img src="themes/images//LARGE 2.svg" alt="Codetikki">CodeTikki</a>
+          <p>Join CodeTikki and test your coding skills with our exciting quizzes.</p>
+      </div>
+      <div class="col-md-3 ms-3">
+           <h4>Quick Links</h4>
+           <p>Home</p>
+      </div>
+      <div class="col-md-3 ms-3">        
+        <h4>CONNECT</h4>
+        <p>Facebook</p>
+        <p>Instagram</p>
+        <p>Youtube</p>
+      </div>
+    </div>
+    <div class="row codetikki">
+      <div class="col">CodeTikki 2024</div>
+    </div>
+  </footer >
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</html>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-</body>
+  </body>
 </html>
-
---
+++ login.php
@@ -0,0 +1,99 @@
+<?php
+include 'setting.php';
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = isset($_POST['text']) ? $_POST['text'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
+
+    function fetchUserID($name, $pdo) {
+      try {
+          $stmt = $pdo->prepare("SELECT Id FROM User WHERE username = :username ");
+          $stmt->bindParam(':username', $name);
+          $stmt->execute();
+          $user_id = $stmt->fetchColumn();
+          return $user_id;
+      } catch(PDOException $e) {
+          echo "Error fetching user ID: " . $e->getMessage();
+          return null;
+      }
+  }
+  $user_id = fetchUserID($username, $pdo);
+  $_SESSION['user_id'] = $user_id;
+  //for make a email session 
+  
+    try {
+        $verifyStmt = $pdo->prepare("SELECT * FROM User WHERE username = :username ");
+        $verifyStmt->bindParam(':username', $username);
+        $verifyStmt->execute();
+        $user = $verifyStmt->fetch(PDO::FETCH_ASSOC);
+  
+        if ($user) {
+          $hashed_password_from_db = $user['password'];
+      
+          if (password_verify($password, $hashed_password_from_db)) {
+              $_SESSION['userid'] = $user['Id'];
+              $_SESSION['username'] = $user['username'];
+              $_SESSION['mailid'] = $user['email'];
+              
+              // Redirect to home.php
+              header("Location: home.php");
+              exit();
+          } else {
+              $error_message = "Invalid username or password.";
+          }
+      } else {
+          $error_message = "User not found.";
+      }
+      
+    } catch (\PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login Page</title>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<link href="./themes/css/style.css" rel="stylesheet">
+</head>
+<div class="container-fluid login-page">
+  <div class="row">
+    <div class="col-md-6 logo-pic"><img src="./themes/images/image-removebg-preview (50) 2.png" alt=""></div>
+    <div class="col-lg-6 offset-lg-3">
+      <div class="login-box">
+        <h2 class="text-center mb-4 login-heading">Login Form</h2>
+        <?php if (isset($error_message)) { ?>
+        <p><?php echo $error_message; ?></p>
+    <?php } ?>
+        
+        <form method="post" action="home.php">
+          <div class="form-group form-group_login">
+            <input type="text" id="text" name="text" class="form-control" placeholder="Username">
+          </div>
+          <div class="form-group form-group_login">
+            <input type="password" id="password" name="password" class="form-control" placeholder="password">
+          </div>
+          <div class="form-group form-group_login">
+          <button type="submit" class="btn btn_login">Signin</button>
+          <label class="or" >OR</label>
+                        <button type="button" class="btn btn-register "><a href="registration.php" >Register</a></button>
+                    
+</div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</html>
+
+</body>
+</html>
+
